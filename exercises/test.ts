178fb457f2a592f4a1af6f268d04887ad4546102
Yair <32864499+bluepostit@users.noreply.github.com>
2023-01-03 15:21:51 +0100

    added simple test file

---
+++ exercises/test.ts
@@ -0,0 +1,18 @@
+// Import your functions here...
+
+console.group('extractName:');
+console.log(extractName(artists[0]));
+console.log(extractName(albums[0]));
+console.groupEnd();
+
+console.group('getAlbumsByArtistName:');
+console.log(getAlbumsByArtistName('Led Zeppelin'));
+console.groupEnd();
+
+console.group('getArtistAndAlbumsByArtistName:');
+console.log(getArtistAndAlbumsByArtistName('Madonna'));
+console.groupEnd();
+
+console.group('getArtistWithAlbumNames:');
+console.log(getArtistWithAlbumNames(getArtistAndAlbumsByArtistName('Madonna')));
+console.groupEnd();
