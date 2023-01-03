81037130a7e43d5794516fafdffe158a5ba8eb13
Yair <32864499+bluepostit@users.noreply.github.com>
2023-01-03 15:31:58 +0100

    commented out test code

--- exercises/test.ts
+++ exercises/test.ts
@@ -1,18 +1,19 @@
 // Import your functions here...
+// Then uncomment and run to test them
 
-console.group('extractName:');
-console.log(extractName(artists[0]));
-console.log(extractName(albums[0]));
-console.groupEnd();
+// console.group('extractName:');
+// console.log(extractName(artists[0]));
+// console.log(extractName(albums[0]));
+// console.groupEnd();
 
-console.group('getAlbumsByArtistName:');
-console.log(getAlbumsByArtistName('Led Zeppelin'));
-console.groupEnd();
+// console.group('getAlbumsByArtistName:');
+// console.log(getAlbumsByArtistName('Led Zeppelin'));
+// console.groupEnd();
 
-console.group('getArtistAndAlbumsByArtistName:');
-console.log(getArtistAndAlbumsByArtistName('Madonna'));
-console.groupEnd();
+// console.group('getArtistAndAlbumsByArtistName:');
+// console.log(getArtistAndAlbumsByArtistName('Madonna'));
+// console.groupEnd();
 
-console.group('getArtistWithAlbumNames:');
-console.log(getArtistWithAlbumNames(getArtistAndAlbumsByArtistName('Madonna')));
-console.groupEnd();
+// console.group('getArtistWithAlbumNames:');
+// console.log(getArtistWithAlbumNames(getArtistAndAlbumsByArtistName('Madonna')));
+// console.groupEnd();
