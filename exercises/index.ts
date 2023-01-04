7fe7eba357c776d886de304645b9150d2af9ca62
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-04 17:34:31 +0100

    part 3

--- exercises/index.ts
+++ exercises/index.ts
@@ -2,56 +2,55 @@
 
 interface Artist {
     name: string;
-    category: 'artist';
+    category: "artist";
 }
 
 interface Album {
-    name: string,
-    artistName: string,
-    category: 'album'
+    name: string;
+    artistName: string;
+    category: "album";
 }
 
 const artists: Artist[] = [
     {
-        name: 'Madonna',
-        category: 'artist'
+        name: "Madonna",
+        category: "artist",
     },
     {
-        name: 'Led Zeppelin',
-        category: 'artist'
+        name: "Led Zeppelin",
+        category: "artist",
     },
     {
-        name: 'Earth, Wind, and Fire',
-        category: 'artist'
-    }
+        name: "Earth, Wind, and Fire",
+        category: "artist",
+    },
 ];
 
 let albums: Album[] = [
     {
-        name: 'Like a Virgin',
-        artistName: 'Madonna',
-        category: 'album'
+        name: "Like a Virgin",
+        artistName: "Madonna",
+        category: "album",
     },
     {
-        name: 'Like a Prayer',
-        artistName: 'Madonna',
-        category: 'album'
+        name: "Like a Prayer",
+        artistName: "Madonna",
+        category: "album",
     },
     {
-        name: 'Houses of the Holy',
-        artistName: 'Led Zeppelin',
-        category: 'album'
+        name: "Houses of the Holy",
+        artistName: "Led Zeppelin",
+        category: "album",
     },
     {
-        name: 'In Through the Out Door',
-        artistName: 'Led Zeppelin',
-        category: 'album'
-    }
+        name: "In Through the Out Door",
+        artistName: "Led Zeppelin",
+        category: "album",
+    },
 ];
 
-
 // Task 1
-// Write an extractName function that expects to be passed an object that has a name property 
+// Write an extractName function that expects to be passed an object that has a name property
 // and returns the value of that name property.
 
 function extractName(person: { name: string }): string {
@@ -64,11 +63,11 @@ function extractName(person: { name: string }): string {
 // console.groupEnd();
 
 // Task 2
-// Write a getAlbumsByArtistName function that expects to receive a string as an argument and returns 
+// Write a getAlbumsByArtistName function that expects to receive a string as an argument and returns
 // an array of objects of the Album type whose artistName property matches the argument.
 
 function getAlbumsByArtistName(name: string): Array<Album> {
-    let result = [];
+    let result: Array<Album> = [];
     // console.log('name: ', name);
     for (let i = 0; i < albums.length; i++) {
         // console.log("albums: ", albums[i]);
@@ -80,6 +79,32 @@ function getAlbumsByArtistName(name: string): Array<Album> {
     return result;
 }
 
-console.group('getAlbumsByArtistName:');
-console.log(getAlbumsByArtistName('Led Zeppelin'));
+// console.group('getAlbumsByArtistName:');
+// console.log(getAlbumsByArtistName('Led Zeppelin'));
+// console.groupEnd();
+
+// Task 3
+/*
+Write a getArtistAndAlbumsByArtistName function that expects to receive a string as an argument and returns an array. 
+The array it returns should contain:
+    The one object of the Artist type whose name property matches the argument
+    All of the objects of Album type whose artistName property matches the argument
+To find the objects of the Album type, use the getAlbumsByArtistName function that you wrote.
+*/
+
+function getArtistAndAlbumsByArtistName(name: string): Array<Album | Artist> {
+    let result: Array<Album | Artist> = [];
+    for (let i = 0; i < artists.length; i++) {
+        if (name === artists[i].name) {
+            // console.log("albums[i].name: ", albums[i].artistName);
+            result.push(artists[i]);
+            result.push(...getAlbumsByArtistName(name));
+            break;
+        }
+    }
+    return result;
+}
+
+console.group("getArtistAndAlbumsByArtistName:");
+console.log(getArtistAndAlbumsByArtistName("Madonna"));
 console.groupEnd();
